38d7c442c22873456422d6e4bd75442c961c1f19
NVH-Phong <hoangphong1132000@gmail.com>
2025-11-22 15:59:57 +1100

    font

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,12 +1,10 @@
+import { requiredUser } from "@/hooks/requiredUser";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const OrdersPage = async () => {
-  const user = await currentUser();
-  if (!user) {
-    redirect("/");
-  }
+  await requiredUser();
   return <div>OrdersPage</div>;
 };
 
