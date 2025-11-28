db65e3ced5ea21bc1b22629790864ab13bb2ade9
NVH-Phong <hoangphong1132000@gmail.com>
2025-11-29 09:32:22 +1100

    update

---
+++ sanity/schemaTypes/productType.ts
@@ -0,0 +1,113 @@
+import { TrolleyIcon } from "@sanity/icons";
+import { subscribe } from "diagnostics_channel";
+import { defineField, defineType } from "sanity";
+
+export const productType = defineType({
+  name: "product",
+  title: "product",
+  type: "document",
+  icon: TrolleyIcon,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Product Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "images",
+      title: "Product Images",
+      type: "array",
+      of: [{ type: "image", options: { hotspot: true } }],
+    }),
+    defineField({
+      name: "intro",
+      title: "Product Intro",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "Stl file",
+      title: "Product Name",
+      type: "array",
+      of: [{ type: "image", options: { hotspot: true } }],
+    }),
+    defineField({
+      name: "price",
+      title: "Product Price",
+      type: "number",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "discount",
+      title: "Discount Price",
+      type: "number",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "category",
+      title: "Category",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    }),
+    defineField({
+      name: "stock",
+      title: "Stock",
+      type: "number",
+      validation: (Rule) => Rule.min(0),
+    }),
+    defineField({
+      name: "status",
+      title: "Product Status",
+      type: "string",
+      options: {
+        list: [
+          { title: "New", value: "new" },
+          { title: "Hot", value: "hot" },
+          { title: "Sale", value: "sale" },
+        ],
+      },
+    }),
+    defineField({
+      name: "variant",
+      title: "Product Type",
+      type: "string",
+      options: {
+        list: [
+          { title: "NFC tag", value: "nfc tag" },
+          { title: "File", value: "file" },
+        ],
+      },
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "images",
+      subtitle: "price",
+    },
+    prepare(selection) {
+      const { title, subtitle, media } = selection;
+      const image = media && media[0];
+      return {
+        title: title,
+        subtile: `$${subtitle}`,
+        media: image,
+      };
+    },
+  },
+});
