e3c46ffc9ae15878619102726c57825dd9db5b13
NVH-Phong <hoangphong1132000@gmail.com>
2025-12-06 08:42:19 +1100

    out of stock function

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import PriceView from "./PriceView";
+import AddToCartButton from "./AddToCartButton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
     <div className="group text-sm rounded-lg overflow-hidden">
-      <div className="bg-linear-to-r from-zinc-200 via-zinc-300 to-zinc-200 overflow-hidden">
+      <div className="bg-linear-to-r from-zinc-200 via-zinc-300 to-zinc-200 overflow-hidden relative">
         {product?.images && (
           <Link href={"/product"}>
             <Image
@@ -17,9 +18,16 @@ const ProductCard = ({ product }: { product: Product }) => {
               height={500}
               alt="productImage"
               priority
-              className={`w-full h-72 object-contain overflow-hidden group-hover:scale-105 hoverEffect`}
+              className={`w-full h-72 object-contain overflow-hidden hoverEffect ${product?.stock !== 0 && "group-hover:scale-105"}`}
             />
           </Link>
+        )}
+        {product?.stock === 0 && (
+          <div className="absolute top-0 left-0 w-full h-full bg-darkColor/50 flex items-center justify-center">
+            <p className="text-base text-white font-semibold text-center ">
+              Out of Stock
+            </p>
+          </div>
         )}
       </div>
       <div className="py-3 px-2 flex flex-col gap-1.5 bg-zinc-50 border border-t-0 rounded-lg rounded-tl-none rounded-tr-none">
@@ -30,6 +38,7 @@ const ProductCard = ({ product }: { product: Product }) => {
           price={product?.price}
           discount={product?.discount}
         />
+        <AddToCartButton product={product} />
       </div>
     </div>
   );
