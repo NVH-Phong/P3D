347def90a799a0c4ae401d3ba31f69821d0e89b3
NVH-Phong <hoangphong1132000@gmail.com>
2025-12-06 15:26:14 +1100

    asdfa

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import ImageView from "@/components/ImageView";
+import PriceView from "@/components/PriceView";
 import { getProductBySlug } from "@/sanity/helpers/queries";
 import { notFound } from "next/navigation";
 import React from "react";
@@ -17,7 +18,21 @@ const SingleProductPage = async ({
   return (
     <Container className="py-10 flex flex-col md:flex-row gap-10">
       {product?.images && <ImageView images={product?.images} />}
-      <div className="w-full md:w-1/2 flex gap-5 ">Right</div>
+      <div className="w-full md:w-1/2 flex gap-5 ">
+        <div>
+          <h2 className="text-3xl md:text-4xl font-bold mb-2">
+            {product?.title}
+          </h2>
+          <PriceView
+            price={product?.price}
+            discount={product?.discount}
+            className="text-lg font-bold"
+          />
+        </div>
+        {product?.stock && (
+          <p className="bg-green-100 w-24 text-center">In Stock</p>
+        )}
+      </div>
     </Container>
   );
 };
--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -28,7 +28,14 @@ const ImageView = ({ images = [] }: Props) => {
   return (
     <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
       <AnimatePresence mode="wait">
-        <motion.div className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden">
+        <motion.div
+          key={active?._key}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden"
+        >
           <Image
             src={urlFor(active).url()}
             alt="productImage"
@@ -39,6 +46,23 @@ const ImageView = ({ images = [] }: Props) => {
           />
         </motion.div>
       </AnimatePresence>
+      <div className="grid grid-cols-6 gap-2 h-20 md:h-28">
+        {images?.map((image) => (
+          <button
+            onClick={() => setActive(image)}
+            key={image?._key}
+            className={`border rounded-md overflow-hidden ${active?._key === image?._key ? "ring-1 ring-darkColor" : ""}`}
+          >
+            <Image
+              src={urlFor(image).url()}
+              alt="productImage"
+              width={100}
+              height={100}
+              className="w-full h-auto object-contain"
+            />
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
