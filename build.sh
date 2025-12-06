435d33075f8396a06b2e157169051160acb49c58
hbhungg <hung.ba.huynh@proton.me>
2025-12-06 15:13:37 +1100

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export $(cat .env | grep -v '^#' | xargs)
+docker buildx build --platform linux/arm64 \
+  --build-arg NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY="${NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}" \
+  --build-arg NEXT_PUBLIC_SANITY_DATASET="${NEXT_PUBLIC_SANITY_DATASET}" \
+  --build-arg NEXT_PUBLIC_SANITY_PROJECT_ID="${NEXT_PUBLIC_SANITY_PROJECT_ID}" \
+  -t registry.home/p3d-app:latest .
+
+docker push registry.home/p3d-app:latest
