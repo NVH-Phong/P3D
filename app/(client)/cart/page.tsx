7449b5269937393b1bdb90db58555e7d079bec7d
NVH-Phong <hoangphong1132000@gmail.com>
2025-12-10 21:16:40 +1100

    working

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+"use client";
+import Loading from "@/components/Loading";
+import useCartStore from "@/store";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
 
 const CartPage = () => {
+  const [isClient, setIsClient] = useState(false);
+  const {
+    deleteCartProduct,
+    getTotalPrice,
+    getItemCount,
+    getSubTotalPrice,
+    resetCart,
+  } = useCartStore();
+  const user = useUser();
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  if (!isClient) {
+    return <Loading />;
+  }
   return <div>CartPage</div>;
 };
 
