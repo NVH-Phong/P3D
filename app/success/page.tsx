8b0027baa4b4fa3b9639d0b7da6745bb401bed8d
NVH-Phong <hoangphong1132000@gmail.com>
2025-12-12 09:17:27 +1100

    update

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import useCartStore from "@/store";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import { motion } from "motion/react";
+import { Check } from "lucide-react";
 
 const SuccessPage = () => {
-  return <div>SuccessPage</div>;
+  const searchParams = useSearchParams();
+  const orderNumber = searchParams.get("orderNumber");
+  const sessionId = searchParams.get("session_id");
+  const router = useRouter();
+  const { resetCart } = useCartStore();
+  useEffect(() => {
+    if (!orderNumber && !sessionId) {
+      router.push("/");
+    } else {
+      resetCart();
+    }
+  }, [orderNumber, sessionId, resetCart]);
+  return (
+    <div className="py-10 bg-linear-to-br from-gray-50 to-gray-100 flex items-center justify-center p-4">
+      <motion.div>
+        <motion.div>
+          <Check />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default SuccessPage;
