7c8ab9ebeb8b248ab70adccb2307a5943061a2c1
NVH-Phong <hoangphong1132000@gmail.com>
2025-12-12 10:52:53 +1100

    update with layout

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ export const headerData = [
   { title: "Home", href: "/" },
   { title: "Link swap", href: "/Link swap" },
   { title: "Custom order", href: "/Custom order" },
-  { title: "About me", href: "/About me" },
+  { title: "About Us", href: "/about" },
 ];
 
 export const productType = [
